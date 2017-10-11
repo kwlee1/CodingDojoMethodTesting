4e982e159cb705cd3302c4303a73786101d3fee2
kwlee <keith.won.lee@gmail.com>
2017-10-10 20:19:11 -0700

    method testing

---
+++ project_spec.rb
@@ -0,0 +1,28 @@
+require_relative 'project' # include our Project class in our spec file
+RSpec.describe Project do  
+  before(:each) do 
+    @project1 = Project.new('Project 1', 'description 1','owner 1')
+    @project2 = Project.new('Project 2', 'description 2','owner 2')
+  end
+
+  it 'has a getter and setter for name attribute' do
+    @project1.name = "Changed Name" # this line would fail if our class did not have a setter method
+    expect(@project1.name).to eq("Changed Name") # this line would fail if we did not have a getter or if it did not change the name successfully in the previous line.
+    @project1.owner = "Changed Owner"
+    expect(@project1.owner).to eq("Changed Owner")
+  end 
+  
+  it 'has a method elevator_pitch to explain name and description' do
+    expect(@project1.elevator_pitch).to eq("Project 1, description 1")
+    expect(@project2.elevator_pitch).to eq("Project 2, description 2")
+  end
+  it 'can return the tasks' do 
+    expect(@project1.tasks_method).to eq([])
+  end 
+  it 'can add tasks' do 
+    expect(@project1.add_tasks("test")).to eq(["test"])
+  end 
+  it 'can print tasks' do 
+    expect(@project1.print_tasks).to eq([])
+  end 
+end
